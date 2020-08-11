f614d7f2e6ff185a93d863674a9a3b166afde882
jonDuke <jonduke90@gmail.com>
2020-08-11 16:08:36 -0700

    added a second test

--- projects/ancestor/test_ancestor.py
+++ projects/ancestor/test_ancestor.py
@@ -25,6 +25,29 @@ class Test(unittest.TestCase):
         self.assertEqual(earliest_ancestor(test_ancestors, 9), 4)
         self.assertEqual(earliest_ancestor(test_ancestors, 10), -1)
         self.assertEqual(earliest_ancestor(test_ancestors, 11), -1)
+    
+    '''
+          8
+           \ 
+    1   3   10  11
+     \ / \ /   /
+      4   6   9
+     /   / \ /
+    7   5   2
+    '''
+    def test_earliest_ancestor2(self):
+        test_ancestors = [(1,4), (3,4), (3,6), (10,6), (8,10), (11,9), (4,7), (6,5), (6,2), (9,2)]
+        self.assertEqual(earliest_ancestor(test_ancestors, 1), -1)
+        self.assertEqual(earliest_ancestor(test_ancestors, 2), 8)
+        self.assertEqual(earliest_ancestor(test_ancestors, 3), -1)
+        self.assertEqual(earliest_ancestor(test_ancestors, 4), 1)
+        self.assertEqual(earliest_ancestor(test_ancestors, 5), 8)
+        self.assertEqual(earliest_ancestor(test_ancestors, 6), 8)
+        self.assertEqual(earliest_ancestor(test_ancestors, 7), 1)
+        self.assertEqual(earliest_ancestor(test_ancestors, 8), -1)
+        self.assertEqual(earliest_ancestor(test_ancestors, 9), 11)
+        self.assertEqual(earliest_ancestor(test_ancestors, 10), 8)
+        self.assertEqual(earliest_ancestor(test_ancestors, 11), -1)
 
 if __name__ == '__main__':
     unittest.main()
